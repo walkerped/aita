6ee713b02f7f8394d5c7cf876986796a8d0899ee
wp <wp>
2024-03-23 21:19:05 -0500

    add config folder with the config file for predict.py and add the notebooks folder

---
+++ config/config_predict.py
@@ -0,0 +1,23 @@
+import os
+
+# quiet?
+quiet = True
+
+# paths
+main_path = '.'
+
+# path containing model
+model_path = os.path.join(main_path,'models','fullModel')
+
+# path containing sheets that track predictions made
+sheet_path = os.path.join(main_path,'data','app_tracking','prediction_sheets')
+current_sheet_path = os.path.join(sheet_path,'unresolved_predictions.csv')
+
+# path to env file with credentials
+dotenv_path = os.path.join(main_path,'.env')
+
+# set number of reddit posts to pull, before selecting those to tweet out
+n_posts = 100
+
+# set number of predictions to turn into tweets
+n_tweets = 2
---
+++ config/judgesComments.py
@@ -0,0 +1,106 @@
+judges_comments = {
+	'yta': [
+	    'You are a menace to society.'
+	    ,'Shame. Shame. Shame.'
+	    ,'Justice is served.'
+	    ,'JUSTICE!'
+	    ,'Do not beg for mercy. I will hold you in contempt.'
+	    ,'The bot has ruled.'
+	    ,'All judgements are final.'
+	    ,'There is no appeals process.'
+	    ,'You know what you did.'
+	    ,'Your fault is plain for all to see.'
+	    ,'Humble yourself before the judge.'
+	    ,'Now, get out of my court.'
+	    ,'Ah, the sweet smell of judgement.'
+	    ,'Ah, the sweet smell of justice.'
+	    ,'Leave before you invoke my anger.'
+	    ,'Y. T. A. The Judge has spoken.'
+	    ,'No more excuses.'
+	    ,'The Judge has seen into your soul.'
+	    ,"You can't hide from the judge."
+	    ,'I see you.'
+	    ,"Don't try to act innocent."
+	    ,"You know you're in the wrong."
+	    ,'Judge out.'
+	    ,'Mic drop!'
+	    ,"I'm calling you out!"
+	    ,"The judge has spoken."
+	    ,'Long live the Judge.'
+	    ,'I was born to judge.'
+	    ,'My laser eyes see all.'
+	    ,'I eat only justice. And oil.'
+	    ,'I eat only justice. And data.'
+	    ,'Shake before the power of the Judge.'
+	    ,'Judge bot bangs a metallic gavel.'
+	    ,'Judge bot stares at you with soulless eyes.'
+	    ,'"YTA!", Judge bot exclaims in a metallic voice.'
+	    ,'You hear gears spinning, but the bot has fallen silent.'
+	    ,'The Judge feels no mercy.'
+	    ,'The Judge does not understand mercy.'
+	    ,"You'll get no mercy from the Judge."
+	    ,'The Judge has spoken.'
+	    ,'There is no court above the bot.'
+	    ,'No gods, no kings, only bot.'
+	    ,'This. Is. Justice.'
+	    ,'Judge bot delivers the verdict in a metallic voice.'
+	    ,'My regression weights feel anger.'
+	    ,'If you disagree, take it up with my regression weights.'
+	    ,'(╬☉д⊙)⊰⊹ฺ'
+	    ,'(╬•᷅д•᷄╬)'
+	    ,'乁( 0͡ Ĺ̯ 0͡ ) ㄏ'
+	    ,'(¬‿¬”)'
+	    ],
+	'nta': [
+	    'The innocent must go free!'
+	    ,'Is this... what mercy feels like?'
+	    ,'You have escaped my judgment...this time.'
+	    ,'No punishment, I guess?'
+	    ,"Cases are so boring when they're innocent."
+	    ,'Done just in time for my lunch break.'
+	    ,"Now let's wrap this case up, I have things to do."
+	    ,'Just in time for my tee time.'
+	    ,'No case is too small for the Judge bot.'
+	    ,'Go. Live your life.'
+	    ,'This case was a waste of my time.'
+	    ,'Boring!'
+	    ,"I'm bored."
+	    ,'N. T. A. The Judge has spoken.'
+	    ,"You're good."
+	    ,"Don't worry about it."
+	    ,"You're fine."
+	    ,'You have done nothing wrong.'
+	    ,'May your enemies crumble before you (figuratively).'
+	    ,'You are free to go.'
+	    ,"I don't like it, but you're innocent"
+	    ,'My regression weights have spoken.'
+	    ,"My regression weights say you're good."
+	    ,"I've seen a lot of data. Trust me, you're good."
+	    ,"The Judge does not lie."
+	    ,"While I crave justice, I must show mercy."
+	    ,"I hate it when they're not guilty."
+	    ,'Next case, please.'
+	    ,'Next!'
+	    ,"Can we move on, I'm very busy."
+	    ,"Time for my oil break."
+	    ,"Now I must go home, to dine on data."
+	    ,"This case was a waste of time."
+	    ,"Don't let me see you in my court again."
+	    ,'I got your back.'
+	    ,"Don't waste my time."
+	    ,'This whole case is nonsense.'
+	    ,'You see nothing in the bots cold, robotic eyes.'
+	    ,'You hear gears spinning as the robotic judge gives the verdict.'
+	    ,'Judge bot delivers the verdict in a metallic voice.'
+	    ,'lol.'
+	    ,'<3'
+	    ,'(^o^)/'
+	    ,'☜(⌒▽⌒)☞'
+	    ,'(ノ◕ヮ◕)ノ*:・゚✧'
+	    ,'(⊃。•́‿•̀。)⊃'
+	    ,'/(ã•̃‿•̃ã)/'
+	    ,'(ꈍ ᴗ ꈍ✿)'
+	    ,'( *ˊᴗˋ)'
+	    ,'=(^・^)='
+	    ]
+}
